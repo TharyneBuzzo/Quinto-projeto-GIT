60d504fb2e1c57bd93ee1e6f2321001f054d7500
TharyneBuzzo <160740775+TharyneBuzzo@users.noreply.github.com>
2024-03-20 15:31:57 -0300

    Update AtividadeJavaDiasdasemana.java

--- AtividadeJavaDiasdasemana.java
+++ AtividadeJavaDiasdasemana.java
@@ -11,12 +11,27 @@ public class AtividadeJavaDiasdasemana {
 		System.out.println("informe o dia da semana: ");
 		Diasdasemana= ler.nextInt();
 		
-		if (Diasdasemana> 1-7) {
-			System.out.println("dia valido");
+		if (Diasdasemana == 1) {
+			System.out.println("o dia da semana é domingo");
 		}
-		else if(Diasdasemana == 1-7) {
-			System.out.println("o numero é seis");
+		else if(Diasdasemana == 2) {
+			System.out.println("o dia da semana é segunda");
+		}  
+		else if(Diasdasemana ==3){
+			System.out.println("o dia da semana é terça");
 		}
+		else if(Diadasemana ==4){
+			System.out.println("o dia da semana é quarta");
+		}
+		else if(Diasdasemana ==5){
+			System.out.println("o dia da semana é quinta");
+		}
+		else if(Diasdasemana ==6){
+			System.out.println("o dia da semana é sexta");
+		}
+		else if(Diasdasemana ==7){
+			System.out.println("o dia da semana é sabado")
+				}
 		else {
 			System.out.println("o dia é invalido");
 		}
